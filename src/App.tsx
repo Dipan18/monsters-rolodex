e757ce1f859fba6fccdcf37216993221165fac91
Dipan <dipan188@gmail.com>
2023-08-12 17:32:35 -0400

    Refactored the App component to typescript. Genericized fetch logic and moved it to data.util

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ChangeEvent } from 'react';
+
+import { fetchData } from './utils/data.util';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBar from './components/search-bar/search-bar.component';
 
 import './App.css';
 
+type Monster = {
+  id: string,
+  name: string,
+  email: string
+}
+
 const App = () => {
   const [searchField, setSearchField] = useState('');
-  const [monsters, setMonsters] = useState([]);
+  const [monsters, setMonsters] = useState<Monster[]>([]);
   const [searchResult, setSearchResult] = useState(monsters);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => setMonsters(users));
+    const fetchUsers = async () => {
+      const users = await fetchData<Monster[]>('https://jsonplaceholder.typicode.com/users');
+      setMonsters(users);
+    }
+
+    fetchUsers();
   }, []);
 
   useEffect(() => {
@@ -24,7 +35,7 @@ const App = () => {
     setSearchResult(filteredMonsters);
   }, [monsters, searchField]);
 
-  const onSearchChange = (event) => {
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchField(event.target.value.toLowerCase());
   };
 
